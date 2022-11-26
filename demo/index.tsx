0acbe1861d1491507b3d25be51961463b427ef73
Dolf Barr <mail@dolf.me>
2022-11-26 20:13:26 +0100

    Add release scripts

--- demo/index.tsx
+++ demo/index.tsx
@@ -3,5 +3,6 @@ import { createRoot } from 'react-dom/client'
 import { App } from '../src/index.stories'
 
 const container = document.getElementById('app')
-const root = createRoot(container)
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+const root = createRoot(container!)
 root.render(<App />)
