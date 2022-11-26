0ee999a95959d75cd8eb89277db37922972eb945
Dolf Barr <mail@dolf.me>
2022-11-26 17:30:43 +0100

    Add demo app

---
+++ demo/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = (): React.ReactElement => {
+  return (
+    <div>
+      <p>Demo for React log hook.</p>
+      <p>Please check the console for logs.</p>
+    </div>
+  )
+}
+
+export default App
